57b2483710b83b3b991f9b99fd1da83b237e89b0
ali-m-d <alistair_divorty@hotmail.com>
2020-07-27 09:02:38 +0000

    Added partials

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("trix")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +17,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "stylesheets/application"
+import "../css/application.css"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  config.hosts << "6d199b1424e34b75afc7e772f48a1b9c.vfs.cloud9.eu-west-2.amazonaws.com"
 
   # Do not eager load code on boot.
   config.eager_load = false
